de2fa6e5ddde2754465b8bf38aee3e6a4eaaf7d8
Olu Ayandosu <oayandosu@gmail.com>
2016-05-13 14:07:06 -0700

    Update forwarder detail to include schema

--- src/containers/forwarders/show.js
+++ src/containers/forwarders/show.js
@@ -1,12 +1,16 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-
 import { Tab ,Tabs ,TabList ,TabPanel } from 'react-tabs';
+import { SchemaContainer } from 'zooid-meshblu-device-editor';
 
-import { deleteForwarderByUuid, fetchForwarderByUuid } from '../../actions/forwarders/forwarders-actions'
-import { fetchMyDevices } from '../../actions/device/device-actions'
+import MyDevices from '../../components/MyDevices';
 
-import MyDevices from '../../components/MyDevices'
+import { fetchMyDevices } from '../../actions/device/device-actions';
+import {
+  deleteForwarderByUuid,
+  fetchForwarderByUuid
+} from '../../actions/forwarders/forwarders-actions';
+
 
 const propTypes = {
   dispatch: PropTypes.func.isRequired,
@@ -36,15 +40,19 @@ class ForwardersShow extends React.Component {
   }
 
   render() {
-    const { forwarders, myDevices, routeParams } = this.props
-    const { forwarderUuid } = routeParams
-
+    const {
+      forwarders,
+      myDevices,
+      routeParams,
+      activeForwarderType,
+    } = this.props
 
     if (forwarders.fetching) return <div>Loading...</div>
     if (forwarders.error) return <div>Error: {error.message}</div>
     if (_.isEmpty(forwarders.selected)) return null
 
-    const { name, type } = forwarders.selected
+    const forwarder = forwarders.selected
+    const { name, type } = forwarder
 
     return (
       <div>
@@ -62,7 +70,7 @@ class ForwardersShow extends React.Component {
           </TabList>
 
           <TabPanel>
-            <h2>Configurations</h2>
+            <h2>Configure</h2>
           </TabPanel>
 
           <TabPanel>
@@ -77,10 +85,11 @@ class ForwardersShow extends React.Component {
 
 ForwardersShow.propTypes = propTypes
 
-function mapStateToProps({ forwarders, myDevices }) {
+function mapStateToProps({ forwarders, myDevices, activeForwarderType }) {
   return {
     forwarders,
     myDevices,
+    activeForwarderType,
   }
 }
 
